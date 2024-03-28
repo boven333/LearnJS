c28dda7969f31d53c16fdc17cf1d53a224e7e530
boven333 <boven.1154@gmail.com>
2024-03-28 17:41:10 +0700

    Change target connection
    change all branch local to .env

--- api/putdata.js
+++ api/putdata.js
@@ -4,19 +4,26 @@ const port = 3004;
 const mysql = require("mysql2");
 const app = express(); //app server
 
+require('dotenv').config();
+const PORT = process.env.PORT
+const HOST = process.env.DB_HOST
+const USER = process.env.DB_USER
+const PASSWORD = process.env.DB_PASSWORD
+const DATABASE = process.env.DB_NAME
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "mydb",
-  password: "root",
+  host: HOST,
+  user: USER,
+  database: DATABASE,
+  password: PASSWORD,
 });
 connection.connect((err) =>{
   if(!connection){
     console.log({msg : "Connected failed", err});
   } else {
     console.log({msg : "Connected"});
-  }
-})
+  };
+});
 
 app.get("/users", cors(), (req, res) => {
   const sql = "SELECT * FROM webapp.crud";
@@ -31,6 +38,6 @@ app.get("/test", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
 });
